9b19b929f42da83eaf432f4fa27bed24566b2e9a
Omar Minaya <omiinaya@gmail.com>
2020-08-23 23:58:57 -0400

    establishing a connection and providing feedback.

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+//dependencies
 const http = require('http');
 const express = require('express');
-const clientPath = '${__dirname}/client';
+const socket.io = require('socket.io');
 
+//file serving
 const app = express();
-
-app.use(express.static(clientPath));
-
+app.use(express.static('${__dirname}/client'));
 const server = http.createServer(app);
+const io = socketio(server);
 
+io.on('connection', (socket) => {
+    socket.emit('message', 'Connection has been established.');
+});
+
+//error logging
 server.on('error', (err) => {
     console.log('Server error: ', err);
 });
 
+//listener
 server.listen(8080, () => {
     console.log('Server started on 8080');
 });
