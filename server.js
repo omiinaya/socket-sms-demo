cd6da1d589cb6805243005eb672c023547a7fb0b
Omar Minaya <omiinaya@gmail.com>
2020-08-24 00:00:06 -0400

    fixing typo under dependencies and adding in comments.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //dependencies
 const http = require('http');
 const express = require('express');
-const socket.io = require('socket.io');
+const socketio = require('socket.io');
 
 //file serving
 const app = express();
@@ -9,6 +9,7 @@ app.use(express.static('${__dirname}/client'));
 const server = http.createServer(app);
 const io = socketio(server);
 
+//providing feedback on connection
 io.on('connection', (socket) => {
     socket.emit('message', 'Connection has been established.');
 });
